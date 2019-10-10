5fbd5a01887f34c43a99a55ae8c3df4d9a5252ff
atrishabhsharma <32489990+atrishabhsharma@users.noreply.github.com>
2019-10-11 00:45:37 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -22,30 +22,6 @@ class QRCODE():
 
     def printQR(self):
         text=ab.get() #value store
-        from tkinter import * 
-
-import tkinter as tk
-
-import tkinter.messagebox
-
-from QRcode import QR_print
-
-
-
-class QRCODE(): 
-
-  
-
-    def __init__(self,master):
-
-       self.label= Label(master,text="Enter data").pack()
-       
-       self.entry= Entry(master,textvariable=ab,bg="pink",fg="black").pack() #textvariable is to store entered data
-
-       self.button= Button(master,text="Print",command=self.printQR).pack()
-
-    def printQR(self):
-        text=ab.get() #value store
         #### status bar ####
         status = Label(root,text="processing....",bd=1,relief=SUNKEN,anchor=W)
         status.pack(side=BOTTOM,fill=X)
@@ -56,21 +32,6 @@ class QRCODE():
    
         
 
-
-
-root=tk.Tk() # main pop-up window
-
-ab=tk.StringVar() #textvariable type
-
-root.title("QR-CODE generator")
-
-root.geometry("200x200")
-
-b=QRCODE(root)
-
-# above all this will run
-root.mainloop()
-        QR_print(text)
 
 
 root=tk.Tk() # main pop-up window
